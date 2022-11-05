9d885e71cdc07d6ecc016e0d909b5f5c5fedfe62
brennanjv <brennanjamesvincent@outlook.com>
2022-11-05 07:43:17 +0000

    Added a new file named BandNameGenerator in Cloud9 IDE

---
+++ BandNameGenerator.py
@@ -0,0 +1,14 @@
+# Notes for Day 1
+# print("") will print out what is inside the double quotes. I.E print("Hello") would result in console output: Hello
+# We love stackoverflow.com for syntax errors/troubleshooting
+# \n will create a new line I.E print("Hello\nWorld") will output Hello (new line) World
+# Can combine strings with the "+" . I.E print("Hello" + "World"). 
+# Note: spaces are important!
+# input() function will allow for user input.
+# input("What is your name? ") will output 'What is your name? ' and then wait for user to type in
+# print("Hello" + input("What is your name")) will ask for name and then print out the Hello + the user input (NESTING)
+
+print("Welcome to the Band Name Generator Program")
+city = input("What city did you grow up in?\n")
+pet = input("What would you name your pet?\n")
+print("Your band name would be called " + city + " " + pet)
